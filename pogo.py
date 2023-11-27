7c501172375ef699a5a4f499255e01f796283119
Spunch <137802895+Spunchkin@users.noreply.github.com>
2023-11-27 19:16:03 +0400

    Update pogo.py

--- pogo.py
+++ pogo.py
@@ -3,16 +3,6 @@ from sys import argv
 
 argc = len(argv)
 
-# features:
-"""
-
-it will have 2 commands for now, run and new,
-run will just do: python main.py 
-new will create a new project, it has 1 arg: name
-obv it will have a help command
-
-"""
-
 if argc > 1:
     if argv[1] == "new":
         if argc > 2:
